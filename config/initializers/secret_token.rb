80d6b5f567c728e29c473552f87c6527ee1d2a48
Vikas Reddy <vikasreddy.com@gmail.com>
2012-08-09 11:46:45 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IDya::Application.config.secret_token = 'af5d693b44c638fdb4364ed110c7dc599c93e085ac76f601279b63b6147d1388a2c56e73191ab33a38a9dfd2c95d4c8664153e1ea9ce885ad55989a94b7d010b'
