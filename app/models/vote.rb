9038ff69868583bf2a3ccf8f2e91815e504ac5ef
sivakrishna <sivakrishna.m@imaginea.com>
2012-08-10 11:12:23 +0530

    Id field not require

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,6 +1,5 @@
 class Vote
-  include Mongoid::Document
-  field :id, type: Integer
+  include Mongoid::Document  
   field :idea_id, type: Integer
   field :for_against, type: Boolean
   field :username, type: String
